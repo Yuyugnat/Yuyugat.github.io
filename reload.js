a673cf6e03a2e498ba0c46e2b3b35aa2e625a8aa
Yuyugnat <tanguymace2002@gmail.com>
2021-12-14 15:37:07 +0100

    version 4.01

--- reload.js
+++ reload.js
@@ -1,3 +1,7 @@
 window.matchMedia('(orientation: landscape) and (min-width: 741px)').addListener( () => {
     location.reload();
 });
+
+window.matchMedia('(orientation: portrait),(min-width: 760px)').addListener( () => {
+    location.reload();
+});
